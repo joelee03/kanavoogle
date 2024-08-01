a924d22c9e6fcd97986de43ff26c2112cfa2b3bb
leejoe03 <joelee2003@gmail.com>
2024-08-01 13:10:14 +1000

    cors ip configuration

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:3000', // frontend URL
+    origin: 'http://localhost:5173', // frontend URL
     credentials: true
 }));
 
