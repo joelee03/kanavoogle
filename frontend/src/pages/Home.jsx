a2a085216fd240d1aad91a76a2d1c7a048010eaa
leejoe03 <joelee2003@gmail.com>
2024-08-02 18:10:59 +1000

    updated homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,12 +7,11 @@ const Home = () => {
   const navigate = useNavigate();
 
     const handleNavigation = () => {
-        navigate('/account'); //route to account
+        navigate('/signup'); //route to signup
     };
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-
         <div className="mb-8">
           <h1 className="text-4xl font-bold text-gray-800">Kanavoogle</h1>
         </div>
@@ -22,25 +21,7 @@ const Home = () => {
           variant="primary" size="md" onClick={handleNavigation}
           className="w-full p-3 rounded-full bg-blue-500 text-white font-semibold shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
         >
-          Go to Account
-        </Button>
-        </div>
-
-        <div className="w-full max-w-md">
-        <Button 
-          variant="primary" size="md" onClick={navigate('/login')}
-          className="w-full p-3 rounded-full bg-blue-500 text-white font-semibold shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          Login
-        </Button>
-        </div>
-
-        <div className="w-full max-w-md">
-        <Button 
-          variant="primary" size="md" onClick={navigate('/signup')}
-          className="w-full p-3 rounded-full bg-blue-500 text-white font-semibold shadow-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
-        >
-          Signup
+          Get Started
         </Button>
         </div>
 
@@ -51,7 +32,6 @@ const Home = () => {
             <RedirectButton platform="Facebook" />
           </div>
         </div>
-
     </div>
   )
 }
