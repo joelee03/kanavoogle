4c9d0d16b471c66c1075cc4a0309822bdb0dc7fe
minjaekyeong123 <n10935282@qut.edu.au>
2024-08-12 22:13:17 +1000

    changes to preview

--- frontend/src/pages/Preview.jsx
+++ frontend/src/pages/Preview.jsx
@@ -9,7 +9,23 @@ function Preview() {
         <Link to="/"><img className="h-[5rem] object-cover" src={Icon} alt="kanavoogle Icon"/></Link>
       </div>
       <div className='top'>
-        <h1>We Offer</h1>
+        <h1>What we Offer</h1>
+        <p>Kanavoogle Pty Ltd is a start-up company established in 2018, dedicated to providing education 
+          and project management consulting services to tech startups and early childhood education institutions. 
+          Our project aims to develop a digital platform that allows users to inquire about our services. 
+          The platform should be simple, user-friendly, and accessible on both mobile devices and desktops.</p>
+      </div>
+      <div className='top'>
+        <h1>Novel Blockchain Projects</h1>
+      </div>
+      <div className='top'>
+        <h1>21st Century Skills Development</h1>
+      </div>
+      <div className='top'>
+        <h1>Montessori Education Implementation</h1>
+      </div>
+      <div className='top'>
+        <h1>What we Offer</h1>
       </div>
     </div>
   )
