a109004342e8a1ed8d30e57fbba1500c2d03228b
leejoe03 <joelee2003@gmail.com>
2024-08-16 23:56:12 +1000

    implemented logout feature

--- frontend/src/components/sidebar.jsx
+++ frontend/src/components/sidebar.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { HomeIcon, ClipboardDocumentListIcon } from '@heroicons/react/24/outline';
+import { useNavigate  } from 'react-router-dom'
+import Button from '../ui/button'
 import Logo from '../assets/webp/main_logo.webp'
-import Icon from '../assets/webp/icon.webp'
 
 const Sidebar = () => {
+    const navigate = useNavigate();
+
+    const handleNavigation = (route) => {
+        console.log('Navigating to:', route);
+        navigate(route); 
+    };
     return (
         <div className="h-screen w-64 bg-white shadow-md fixed left-0 top-0 flex flex-col justify-between">
             <div className="p-6">
@@ -27,9 +34,14 @@ const Sidebar = () => {
             
             {/* Bottom Section */}
             <div className="p-6">
-                <a href="#" className="flex items-center space-x-2 text-gray-700 hover:text-black">
-                    <img src={Icon} alt="User Icon" className="h-6 w-6 rounded-full" /> 
-                </a>
+                <Button
+                variant="primary"
+                size="sm"
+                onClick={() => handleNavigation('/')}
+                className="w-full p-3 rounded-full text-white items-center justify-center hover:bg-[#7AA647]"
+                >
+                    Logout
+                </Button>
             </div>
         </div>
     );
