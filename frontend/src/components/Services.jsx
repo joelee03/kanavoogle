7fc3520fdc44d9328b427e58bacf5f4db317bed6
leejoe03 <joelee2003@gmail.com>
2024-08-18 22:01:17 +1000

    hero page updates

--- frontend/src/components/Services.jsx
+++ frontend/src/components/Services.jsx
@@ -1,46 +1,40 @@
-import React from 'react'
-import Icon from '../assets/webp/icon.webp'
+import React from 'react';
+import { FaProjectDiagram, FaGraduationCap, FaChild } from 'react-icons/fa'; 
 
 function Services() {
     return (
         <div className='w-full py-[10rem] px-4 bg-white'>
           <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
               <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                  <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Icon} alt="/" />
-                  <h2 className='text-2xl font-bold text-center py-8'>Novel Blockchain Projects</h2>
-                  <p className='text-center text-4xl font-bold'>$149</p>
-                  <div className='text-center font-medium'>
-                      <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                      <p className='py-2 border-b mx-8'>1 Granted User</p>
-                      <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-                  </div>
-                  <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                  <FaProjectDiagram className='w-20 h-20 mx-auto mt-[-3rem] text-blue-500' />
+                  <h2 className='text-2xl font-bold text-center py-8 border-b'>Novel Blockchain Projects</h2>
+                  <p className='text-center text-xl font-medium mx-8 mt-8 pb-8 border-b'>
+                      For evaluating and experimenting with blockchain technology.
+                  </p>
+                  <button className='bg-[#8ABE53] text-white w-[200px] rounded-md font-medium my-9 mx-auto px-6 py-3'>Learn More</button>
               </div>
+
               <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                  <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Icon} alt="/" />
-                  <h2 className='text-2xl font-bold text-center py-8'>21st Century Skills</h2>
-                  <p className='text-center text-4xl font-bold'>$149</p>
-                  <div className='text-center font-medium'>
-                      <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                      <p className='py-2 border-b mx-8'>1 Granted User</p>
-                      <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-                  </div>
-                  <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                  <FaGraduationCap className='w-20 h-20 mx-auto mt-[-3rem] text-green-500' />
+                  <h2 className='text-2xl font-bold text-center py-8 border-b'>Gain 21st Century Skills</h2>
+                  <p className='text-center text-xl font-medium mx-8 mt-8 pb-8 border-b'>
+                      Customized projects for learning top 21st-century skills using Blockchain and AI.
+                  </p>
+                  <button className='bg-[#8ABE53] text-white w-[200px] rounded-md font-medium my-9 mx-auto px-6 py-3'>Learn More</button>
               </div>
+
               <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
-                  <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Icon} alt="/" />
-                  <h2 className='text-2xl font-bold text-center py-8'>Montessori Education</h2>
-                  <p className='text-center text-4xl font-bold'>$149</p>
-                  <div className='text-center font-medium'>
-                      <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                      <p className='py-2 border-b mx-8'>1 Granted User</p>
-                      <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
-                  </div>
-                  <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3'>Start Trial</button>
+                  <FaChild className='w-20 h-20 mx-auto mt-[-3rem] text-purple-500' />
+                  <h2 className='text-2xl font-bold text-center py-8 border-b'>Montessori Education</h2>
+                  <p className='text-center text-xl font-medium mx-8 mt-8 pb-8 border-b'>
+                      Expansion of day care services with Montessori education model.
+                  </p>
+                  <button className='bg-[#8ABE53] text-white w-[200px] rounded-md font-medium my-9 mx-auto px-6 py-3'>Learn More</button>
               </div>
           </div>
         </div>
-      );
+    );
 }
 
-export default Services
+export default Services;
+
