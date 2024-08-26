4b4ca560a11022087d46353e37e7f4b6306ee3f4
leejoe03 <joelee2003@gmail.com>
2024-08-26 18:40:30 +1000

    added courses

--- frontend/src/ui/navbar.jsx
+++ frontend/src/ui/navbar.jsx
@@ -15,25 +15,28 @@ const Navbar = () => {
   return (
     <header className="bg-white">
       <div className="container max-w-[1400px] mx-auto p-2.5 flex items-center justify-between">
-        <Link to="/" className="text-gray-800 no-underline">
-          <img className="h-12 object-cover mr-10" src={Logo} alt="kanavoogle logo" /> 
-        </Link>
+        <div className="flex items-center">
+          <Link to="/" className="text-gray-800 no-underline">
+            <img className="h-12 object-cover" src={Logo} alt="kanavoogle logo" />
+          </Link>
+          <Link to="/courses" className="ml-10 text-gray-800 no-underline hover:text-[#7AA647]">
+            Courses
+          </Link>
+        </div>
         <nav className="flex items-center">
-          {user && (
+          {user ? (
             <div className="flex items-center">
               <span className="text-gray-800">{user.email}</span>
               <button 
                 onClick={handleClick} 
-                className="ml-2.5 bg-white text-green-500 border-2 border-green-500 p-1.5 rounded font-poppins text-base"
-              >
-                Log out
+                className="ml-2.5 bg-white text-[#7AA647] border-2 border-[#7AA647] p-1.5 rounded font-poppins">
+                Logout
               </button>
             </div>
-          )}
-          {!user && (
-            <div className="flex items-center">
-              <Link to="/login" className="ml-2.5 text-gray-800 no-underline">Login</Link>
-            </div>
+          ) : (
+            <Link to="/login" className="bg-[#7AA647] text-white p-2 rounded">
+              Sign in
+            </Link>
           )}
         </nav>
       </div>
