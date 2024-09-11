e567dbe1e548f7a6868265c32bb177b3d31493c5
leejoe03 <joelee2003@gmail.com>
2024-09-11 18:53:56 +1000

    fixed logout bug

--- frontend/src/ui/navbar.jsx
+++ frontend/src/ui/navbar.jsx
@@ -14,6 +14,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       await signOut(auth);
+      window.location.reload();
     } catch (error) {
       console.error("Error logging out:", error);
     }
